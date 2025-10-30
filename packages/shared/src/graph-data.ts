db182e247ae335012e636da9fdf828327c272357
Artyom Prikhodko <homoogenecus@gmail.com>
2025-10-30 20:18:00 +0100

    add shared types

---
+++ packages/shared/src/graph-data.ts
@@ -0,0 +1,36 @@
+export interface GraphXYPosition {
+  x: number;
+  y: number;
+}
+
+export interface GraphNode<T extends object> {
+  id: string;
+  type: string;
+
+  position: GraphXYPosition;
+
+  data: T;
+
+  hidden?: boolean;
+  selected?: boolean;
+}
+
+export interface GraphEdge<
+  T extends Record<string, unknown> = Record<string, unknown>
+> {
+  id: string;
+
+  source: string;
+  target: string;
+
+  data?: T;
+  type?: string;
+  hidden?: boolean;
+  selected?: boolean;
+  animated?: boolean;
+}
+
+export interface GraphData<T extends object> {
+  nodes: GraphNode<T>[];
+  edges: GraphEdge[];
+}
---
+++ packages/shared/src/index.ts
@@ -0,0 +1,23 @@
+export type {
+  Beat,
+  BeatPartial,
+  Effect,
+  Transition,
+  Goto,
+  BeatWithValidation,
+} from './beat';
+export type {
+  NHellEvent,
+  NHellChoice,
+  NHellEffect,
+  StoryMetadata,
+  StoryData,
+  NHellEventType,
+  NHellEffectType,
+} from './story';
+export type {
+  GraphData,
+  GraphEdge,
+  GraphNode,
+  GraphXYPosition,
+} from './graph-data';
